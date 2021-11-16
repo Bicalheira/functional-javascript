e74cfee3599be614b57d446706b19f475c79aba9
Lucas Bicalho <desenvolvimento7@arcoinformatica.com>
2021-11-16 09:01:24 -0300

    Realizado novo util e organizando melhor o código

--- public/app/app.js
+++ public/app/app.js
@@ -1,7 +1,14 @@
-import { handleStatus } from "./utils/promise-helpers.js";
+import { handleStatus, log } from "./utils/promise-helpers.js";
+import "./utils/array-helpers.js";
 
 document.querySelector("#myButton").onclick = () =>
 	fetch("http://localhost:3000/notas")
 		.then(handleStatus)
-		.then((invoices) => console.log(invoices))
+		.then((invoices) =>
+			invoices
+				.$flatMap((invoice) => invoice.itens)
+				.filter((item) => item.codigo == "2143")
+				.reduce((total, item) => total + item.valor, 0)
+		)
+		.then(console.log)
 		.catch(console.log);
--- public/app/utils/promise-helpers.js
+++ public/app/utils/promise-helpers.js
@@ -1,1 +1,6 @@
 export const handleStatus = (response) => (response.ok ? response.json() : Promise.reject(response.statusText));
+
+export const log = (param) => {
+	console.log(param);
+	return param;
+};
